6ebecbd9f71851c0a27dd8a02f4c4bbd5b6b454e
EmiloGaras <EmiloGaras@EmiloGaras-PC>
2017-11-19 21:52:41 -0200

    Versão 2.5

--- src/main/resources/db/migration/v01__elementos_categoria.sql
+++
@@ -1,8 +0,0 @@
-
-
-INSERT INTO categoria (nome) values ('Lazer');
-INSERT INTO categoria (nome) values ('Alimentacao');
-INSERT INTO categoria (nome) values ('Apresentacao');
-INSERT INTO categoria (nome) values ('Supermercado');
-INSERT INTO categoria (nome) values ('Farmacia');
-INSERT INTO categoria (nome) values ('Outros');
